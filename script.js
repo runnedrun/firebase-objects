9fcf44e21d9f76e2a2d3402d5f90adb9e48d0ff3
David Gaynor <runnedrun@gmail.com>
2016-10-21 08:21:22 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -12,7 +12,7 @@
 
 onNewListItem("product-list", function(product) {
   var nameHeader = $("<h1 class='product-name'>" + product.name + "</h1>")
-  var image = $("<img class='product-image'>").attr("src")
+  var image = $("<img class='product-image'>").attr("src", "http://pix.iemoji.com/images/emoji/apple/ios-9/256/thinking-face.png")
 
   $(".product-list-container").append(nameHeader, priceHeader, image, detailsList)
 })
